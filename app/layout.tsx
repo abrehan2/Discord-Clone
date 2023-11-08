a340acedb7e5fc1ecf0b65330672cb5d3fc75b95
abrehan2 <rehannajam2@gmail.com>
2023-11-08 22:08:49 +0500

    Enabled dark mode functionality with the next-themes library

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import "./globals.css";
 
 const font = Open_Sans({ subsets: ["latin"] });
@@ -18,8 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={font.className}>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="discord-clone">{children}</ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
